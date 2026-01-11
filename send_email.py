ab7d295a4fd5b6f861ccaa39e96d65f0478ba38c
Srujan-KN <srujangowda762@gmail.com>
2026-01-11 14:48:43 +0530

    feat: Added script to send email

---
+++ send_email.py
@@ -0,0 +1,28 @@
+# For testing, you can hardcode first
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+smtp_server = "smtp.gmail.com"
+port = 587
+username = "brucewayne76211@gmail.com"
+password = "your_app_password"
+
+to_email = "srujangowda762@gmail.com"
+subject = "Test Email"
+body = "Hello! This is a test email from Python."
+
+# Create message
+msg = MIMEText(body, _charset="utf-8")
+msg["From"] = username
+msg["To"] = to_email
+msg["Subject"] = subject
+msg["Date"] = formatdate(localtime=True)  # Adds date header
+
+# Send email
+with smtplib.SMTP(smtp_server, port) as server:
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(username, [to_email], msg.as_string())
+
+print("Email sent successfully!")
